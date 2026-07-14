8140acd6b1d65f5e46ac867c5ca02ae1acbcc4da
YevhenVe <eugene.veprytskyi@gmail.com>
2026-07-14 13:35:36 -0400

    locale detection false

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ export const routing = defineRouting({
   locales: ["uk", "ru"],
   defaultLocale: "uk",
   localePrefix: "as-needed",
+  localeDetection: false,
 });
 
 export type AppLocale = (typeof routing.locales)[number];
